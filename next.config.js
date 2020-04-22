a1b4ae3a432dd4154be7e8d841df853dbee6f1ec
Matteo Depalo <matteo@depalo.me>
2020-04-22 16:29:48 +0100

    Test client
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+module.exports = phase => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isProd = phase === PHASE_PRODUCTION_BUILD
+
+  const env = {
+    API_URL: (() => {
+      if (isDev) return 'http://localhost:3001'
+      if (isProd) { return 'https://focaccia-client.now.sh' }
+      return 'http://localhost:3001'
+    })()
+  }
+
+  return {
+    env
+  }
+}
