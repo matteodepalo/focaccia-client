84906d9cf8cd39dda00f2231053e73f4b06ad208
Matteo Depalo <matteo@depalo.me>
2020-04-26 17:03:11 +0100

    Use config
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { GetServerSideProps } from 'next'
 import axios from 'axios'
+import config from '../next.config'
 
 type Props = {
   number: number
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async _context => {
-  const response = await axios.get(process.env.API_URL!)
+  const response = await axios.get(config().env.API_URL)
   const number = +response.data
 
   return { props: { number } }
