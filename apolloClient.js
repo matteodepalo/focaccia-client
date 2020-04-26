35c40fcf819f6d4187b1c6e65f0fd52c49c37f1b
Matteo Depalo <matteo@depalo.me>
2020-04-26 21:24:04 +0100

    JS
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- apolloClient.js
+++ apolloClient.js
@@ -1,19 +1,18 @@
 import { ApolloClient } from 'apollo-client'
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import config from './next.config'
-import { NextPageContext } from 'next';
 import fetch from 'isomorphic-unfetch'
 
-export default function createApolloClient(initialState: NormalizedCacheObject, ctx: NextPageContext) {
+export default function createApolloClient(initialState, ctx) {
   // The `ctx` (NextPageContext) will only be present on the server.
   // use it to extract auth headers (ctx.req) or similar.
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
-      uri: config().env.API_URL, // Server URL (must be absolute)
+      uri: process.env.API_URL, // Server URL (must be absolute)
       fetch
     }),
-    cache: new InMemoryCache().restore(initialState),
+    cache: new InMemoryCache().restore(initialState)
   })
 }
