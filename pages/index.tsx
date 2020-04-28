f2eb736ec801627b0dd4e2dbedd04cdd365b223d
Matteo Depalo <matteo@depalo.me>
2020-04-28 21:13:55 +0100

    Add blueprint
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { withApollo } from '../lib/apollo'
-import RecipeForm from '../components/RecipeForm';
-import RecipeList from '../components/RecipeList';
+import RecipeForm from '../components/RecipeForm'
+import RecipeList from '../components/RecipeList'
+import styles from './index.module.css'
 
 const Home = () => {
   return (
-    <section>
+    <section className={styles.mainSection}>
+      <h1>Recipes</h1>
       <RecipeList/>
       <RecipeForm/>
     </section>
