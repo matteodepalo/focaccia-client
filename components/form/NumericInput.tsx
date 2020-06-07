f841cac798e2587248e9f07d105480726ec7dc21
Matteo Depalo <matteo@depalo.me>
2020-06-07 13:09:06 +0100

    Impove numeric input
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- components/form/NumericInput.tsx
+++ components/form/NumericInput.tsx
@@ -2,22 +2,41 @@ import { NumericInput as BPNumericInput } from '@blueprintjs/core'
 import { FunctionComponent } from "react"
 import { Box, BoxProps } from "rebass/styled-components"
 import { lte } from 'lodash'
+import { FormikHelpers, FieldProps } from 'formik'
 
 interface Props {
+  name?: string
+  value: number
+  onChange: (value: number) => void
+  onBlur?: FieldProps<number>['field']['onBlur']
+  validateField?: FormikHelpers<any>['validateField']
   boxProps?: BoxProps
   inputProps: BPNumericInput['props']
 }
 
 
-export const NumericInput: FunctionComponent<Props> = ({ boxProps, inputProps }) => {
+export const NumericInput: FunctionComponent<Props> = ({ value, name, onChange, onBlur, validateField, boxProps, inputProps }) => {
+  const handleNumericInputChange = (valueAsNumber: number, valueAsString: string) => {
+    if (!isNaN(valueAsNumber) && valueAsString.length > 0) {
+      onChange(valueAsNumber)
+    } else {
+      onChange(0)
+    }
+
+    name && validateField?.(name)
+  }
+
   return (
     <Box sx={{ touchAction: 'manipulation' }} {...boxProps}>
       <BPNumericInput
         {...inputProps}
+        name={name}
         allowNumericCharactersOnly={true}
         fill={true}
+        onValueChange={handleNumericInputChange}
+        onBlur={onBlur}
         min={inputProps.min ?? 0}
-        value={lte(inputProps.value, 0) ? undefined : inputProps.value}
+        value={lte(value, 0) ? undefined : value}
         placeholder='0'/>
     </Box>
   )
