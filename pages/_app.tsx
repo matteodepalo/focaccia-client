4e0936cedb540edce98813e40be4e82b4fa7e9cb
Matteo Depalo <matteo@depalo.me>
2020-06-09 17:07:14 +0100

    Use Nunito sans
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ const GlobalStyle = createGlobalStyle<DefaultTheme>`
   body {
     background-color: ${(props) => props.theme.backgroundColor};
     color: ${(props => props.theme.textColor)};
+    font-family: 'Nunito';
   }
 
   .${Classes.HEADING} {
