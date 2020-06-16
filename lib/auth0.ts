700610f613a09b9275cf46de88374fe3e501fb64
Matteo Depalo <matteo@depalo.me>
2020-06-16 18:50:57 +0100

    Remove build env from vercel.json
    Signed-off-by: Matteo Depalo <matteo@depalo.me>

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,13 +1,13 @@
 import { initAuth0 } from '@auth0/nextjs-auth0'
 
 export default initAuth0({
-  clientId: process.env.AUTH0_CLIENT_ID || 'http://localhost:3001/graphql',
+  clientId: process.env.AUTH0_CLIENT_ID!,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: 'openid profile offline_access',
-  domain: process.env.AUTH0_DOMAIN || 'focaccia.eu.auth0.com',
-  redirectUri: process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
+  domain: process.env.AUTH0_DOMAIN!,
+  redirectUri: process.env.REDIRECT_URI!,
   audience: process.env.API_URL,
-  postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000',
+  postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI!,
   session: {
     cookieSecret: process.env.SESSION_COOKIE_SECRET!,
     cookieLifetime: parseInt(process.env.SESSION_COOKIE_LIFETIME || "7200", 10),
